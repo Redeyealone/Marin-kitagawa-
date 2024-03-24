0825a10a203443c70f53d6eb60ef2d97533b41fe
dilshadhasnain13 <162882004+dilshadhasnain13@users.noreply.github.com>
2024-03-24 12:19:17 +0545

    Update disaster.py

--- Mikobot/plugins/disaster.py
+++ Mikobot/plugins/disaster.py
@@ -25,7 +25,7 @@ from Mikobot.plugins.log_channel import gloggable
 
 ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "Mikobot/elevated_users.json")
 
-def check_user_id(user_id: int, context: ContextTypes) -> str:
+def check_user_id(user_id: int, context: CallbackContext) -> str:
     bot = context.bot
     if not user_id:
         reply = "That...is a chat! baka ka omae?"
