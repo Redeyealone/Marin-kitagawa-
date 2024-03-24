9f9ba7408c7e3ed8b69ba95ccb6a2f151193b342
dilshadhasnain13 <162882004+dilshadhasnain13@users.noreply.github.com>
2024-03-24 13:33:43 +0545

    Update disaster.py

--- Mikobot/plugins/disaster.py
+++ Mikobot/plugins/disaster.py
@@ -42,7 +42,7 @@ def check_user_id(user_id: int, context: ContextTypes) -> Optional[str]:
 
 @dev_plus
 @gloggable
-def addsudo(update: Update, context: ContextTypes) -> str:
+def addsudo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     message = update.effective_message
     user = update.effective_user
     chat = update.effective_chat
