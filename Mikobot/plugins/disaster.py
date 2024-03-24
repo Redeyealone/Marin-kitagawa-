066d96cc67bf9a1aeef7b636148fac6c753860e2
dilshadhasnain13 <162882004+dilshadhasnain13@users.noreply.github.com>
2024-03-24 10:18:24 +0545

    Update disaster.py

--- Mikobot/plugins/disaster.py
+++ Mikobot/plugins/disaster.py
@@ -3,9 +3,7 @@ import json
 import os
 from typing import Optional
 
-from telegram import ParseMode, TelegramError, Update
-from telegram.ext import CallbackContext, CommandHandler
-from telegram.utils.helpers import mention_html
+
 
 from Mikobot import (
     DEMONS,
