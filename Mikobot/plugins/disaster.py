2c54a1392bfd48dd4be199a4d551a99e5d2459fb
dilshadhasnain13 <162882004+dilshadhasnain13@users.noreply.github.com>
2024-03-24 12:59:43 +0545

    Update disaster.py

--- Mikobot/plugins/disaster.py
+++ Mikobot/plugins/disaster.py
@@ -593,7 +593,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "Dᴇᴠꜱ"
+__mod_name__ = "DEVS"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
