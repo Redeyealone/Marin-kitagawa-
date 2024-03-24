5372dd7e06d98d7a06f3cce3b87210fb859627ca
dilshadhasnain13 <162882004+dilshadhasnain13@users.noreply.github.com>
2024-03-24 08:30:50 +0545

    Update tagall.py

--- Mikobot/plugins/tagall.py
+++ Mikobot/plugins/tagall.py
@@ -5,7 +5,6 @@ from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from Mikobot import telethn as client
 
 spam_chats = []
 
