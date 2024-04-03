a4b07b0651931061687911eb5ad79b3bb94760e9
dilshadhasnain13 <162882004+dilshadhasnain13@users.noreply.github.com>
2024-04-03 06:28:12 +0545

    Update whisper.py

--- Mikobot/plugins/whisper.py
+++ Mikobot/plugins/whisper.py
@@ -1,4 +1,4 @@
-from Mikobot import pbot as pgram, BOT_USERNAME
+from Mikobot import tbot as pgram, BOT_USERNAME
 from pyrogram import filters
 from pyrogram.types import (
     InlineQueryResultArticle, InputTextMessageContent,
